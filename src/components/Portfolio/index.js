aaa36cad9ede492bd4d08f1656049c12a39b169c
ElK <62822011+evkonradi@users.noreply.github.com>
2020-11-18 21:16:55 -0600

    grid for projects is changed for new layout

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,45 +1,60 @@
 import React, { useState } from "react";
+import imgTeactMeTo from '../../assets/projects/teach-me-to.jpg';
+import imgCardSocial from '../../assets/projects/cardsocial.png';
+import imgRentz from '../../assets/projects/rentzsite.jpg';
+import imgBadgetTracker from '../../assets/projects/badget-tracker.jpg';
+import imgTectBlog from '../../assets/projects/tech-blog.jpg';
+import imgCommerceAPI from '../../assets/projects/e-commerce-APIs.jpg';
+import imgWeather from '../../assets/projects/weather-dashboard.jpg';
 
 function Portfolio() {
 
     const [projects] = useState([
         {
-            fileName : './assets/projects/cardsocial.png',
+            fileName : imgTeactMeTo,
+            // fileName : 'teach-me-to.jpg',
+            title: 'Teach Me To',
+            technology: 'Mongo|Express|React|Node|GraphQL|PWA',
+            githubLink: 'https://github.com/evkonradi/teach-me-to',
+            deployedLink: 'https://floating-spire-39046.herokuapp.com/'
+        },
+        {
+            fileName : imgCardSocial,
             title: 'Card Social',
             technology: 'Node.js|Express|MySQL|Sequelize|MVC|JavaScript',
             githubLink: 'https://github.com/evkonradi/CardSocial',
             deployedLink: 'https://cryptic-shelf-96558.herokuapp.com/'
         },
         {
-            fileName : './assets/projects/rentzsite.jpg',
+            fileName : imgRentz,
             title: 'Rentz Rentals',
             technology: 'Server APIs|JavaScript|CSS Foundation',
             githubLink: 'https://github.com/evkonradi/Rentz',
             deployedLink: 'https://evkonradi.github.io/Rentz/'
         },
         {
-            fileName : './assets/projects/badget-tracker.jpg',
+            fileName : imgBadgetTracker,
             title: 'Badget Tracker',
             technology: 'Node|Express|MongoDB|PWA|Heroku|Atlas MongoDB',
             githubLink: 'https://github.com/evkonradi/badget-tracker',
             deployedLink: 'https://arcane-ravine-44020.herokuapp.com/'
         },
         {
-            fileName : './assets/projects/tech-blog.jpg',
+            fileName : imgTectBlog,
             title: 'Tech Blog',
             technology: 'Node|Express|MySQL|MVC|ORM',
             githubLink: 'https://github.com/evkonradi/techBlog',
             deployedLink: 'https://immense-reaches-05057.herokuapp.com/'
         },
         {
-            fileName : './assets/projects/e-commerce-APIs.jpg',
+            fileName : imgCommerceAPI,
             title: 'E-Commerce Server-side APIs',
             technology: 'Node|Express|MySQL|Insomnia',
             githubLink: 'https://github.com/evkonradi/e-commerce',
             deployedLink: 'https://drive.google.com/file/d/11fDNyAmj2zRqzddM2fHlUWQJWdoG6hqJ/view?usp=sharing'
         },
         {
-            fileName : './assets/projects/weather-dashboard.jpg',
+            fileName : imgWeather,
             title: 'Weather Dashboard',
             technology: 'HTML|JavaScript|Bootstrap|Server-side APIs',
             githubLink: 'https://github.com/evkonradi/WeatherDashboard',
@@ -58,6 +73,19 @@ function Portfolio() {
             </p>
             </div>
             <div className="section-right work-grid">
+                {projects.map((project, i) => (
+                        <article key={project.title} className={`work-grid-item work-grid-item-${i+2}`}>
+                            <img src={project.fileName} alt={project.title} className="work-grid-item-img"></img>
+                            <p className="work-card-title">{project.title}</p>
+                            <p className="work-card-subtitle">{project.technology}</p>
+                            <p className="work-card-subtitle">{project.githubLink}</p>
+                            <p className="work-card-subtitle">{project.deployedLink}</p>
+                        </article>
+            ))}
+            </div>
+
+
+            {/* <div className="section-right work-grid">
                 <article className="work-grid-item work-grid-item-1">
                     <div className="card work-card-flex">
                         <a href = "https://github.com/evkonradi/teach-me-to" className="work-card-link">
@@ -76,7 +104,7 @@ function Portfolio() {
                             </div>
                         </article>
             ))}
-            </div>
+            </div> */}
 
         </section>
     )
